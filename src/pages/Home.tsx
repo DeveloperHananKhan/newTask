9bd39fa09a1057b960094f56d8f782bcb2d5c353
Hanan khan <hkshawls956@gmail.com>
2025-10-28 15:12:24 +0530

    df

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,11 +7,11 @@ export const Home = () => {
   return (
     <>
       
-        <div className=" flex w-full  min-w-0  overflow-hidden">
+        <div className=" flex w-full overflow-hidden">
           <div>
             <SideBar />
           </div>
-          <div className="flex-1 flex flex-col">
+          <div className="flex-1 flex flex-col w-full">
             <Header />
             <div className="px-4 ">
               <TaskCat />
